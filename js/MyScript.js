8f6d39d64cb91310b96acb034ddb7c159e395478
André Monteiro <andremgmonteiro01@gmail.com>
2022-09-13 12:16:51 +0100

    Tried fixing some sizes and positions

--- js/MyScript.js
+++ js/MyScript.js
@@ -1,11 +1,14 @@
 addEventListener('scroll', (event) => {
+    if(innerWidth < '768') return;
+
     const header = document.getElementById("header");
     const headerSticky = document.getElementById("headerSticky");
 
-    if(window.scrollY > 1)
+    if(window.scrollY > 200)
     {
         header.classList.add('fadeIn');
         header.classList.remove('fadeOut');
+        header.classList.remove('fadeInOut');
         headerSticky.classList.add('fadeOutDown');
         headerSticky.classList.remove('fadeInUp');
         headerSticky.classList.remove('fadeInUpFast');
